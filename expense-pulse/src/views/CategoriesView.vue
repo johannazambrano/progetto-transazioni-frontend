795947a997b614bdf14f1b0b129587b2cc81b1ea
Johanna Zambrano <j.zambrano@tdnet.it>
2026-01-26 16:11:01 +0100

    Aggiunta stile al bottone Blocca/modifica layout

--- expense-pulse/src/views/CategoriesView.vue
+++ expense-pulse/src/views/CategoriesView.vue
@@ -21,18 +21,50 @@ const editingCategory = ref<Category | null>(null);
 const isSaving = ref(false);
 const GridContainer = _GridContainer as any;
 
-
 // Mappa dei componenti - chiave: ID componente
 const componentMap: Record<string, Component> = {
   form: CategoryForm,
   table: CategoryTable,
-  stats: CategoryStats
+  stats: CategoryStats,
 };
 
 const layout = ref<LayoutItem[]>([
-  { i: "form", x: 0, y: 0, w: 12, h: 6, minW: 8,maxW: 12,minH: 3,maxH: 3,static: false},
-  { i: "table", x: 0, y: 6, w: 12, h: 10, minW: 4,maxW: 12,minH: 6,maxH: 12,static: false},
-  { i: "stats", x: 0, y: 6, w: 12, h: 4,minW: 10,maxW: 12,minH: 9,maxH: 12,static: false},
+  {
+    i: "form",
+    x: 0,
+    y: 0,
+    w: 12,
+    h: 6,
+    minW: 8,
+    maxW: 12,
+    minH: 3,
+    maxH: 3,
+    static: false,
+  },
+  {
+    i: "table",
+    x: 0,
+    y: 6,
+    w: 12,
+    h: 10,
+    minW: 4,
+    maxW: 12,
+    minH: 6,
+    maxH: 12,
+    static: false,
+  },
+  {
+    i: "stats",
+    x: 0,
+    y: 6,
+    w: 12,
+    h: 4,
+    minW: 10,
+    maxW: 12,
+    minH: 9,
+    maxH: 12,
+    static: false,
+  },
 ]);
 
 const draggable = ref(true);
@@ -60,35 +92,28 @@ const toggleEditMode = () => {
     <header><Header /></header>
 
     <div class="flex justify-end mr-3 mb-4">
-      <button @click="editMode = !editMode" class="btn-style-config">
-        <span>{{ editMode ? 'Blocca Layout' : 'Modifica Layout' }}</span>
-      </button>
-    </div>
-
-    <!-- <div class="flex justify-end mr-3">
-      <button @click="toggleEditMode" :class="[
-        'flex items-center px-2 py-2 rounded-xl text-sm font-medium transition-all',
-        !editMode
-          ? 'bg-indigo-600 text-white shadow-md hover:bg-indigo-700'
-          : 'bg-white border border-gray-200 text-gray-600 hover:border-indigo-200 hover:text-indigo-600 hover:shadow-sm'
-      ]">
+      <button
+        @click="editMode = !editMode"
+        :class="[
+          'flex items-center px-2 py-2 rounded-xl text-sm font-medium transition-all',
+          !editMode
+            ? 'bg-indigo-600 text-white shadow-md hover:bg-indigo-700'
+            : 'bg-white border border-gray-200 text-gray-600 hover:border-indigo-200 hover:text-indigo-600 hover:shadow-sm',
+        ]"
+      >
         <Lock v-if="!editMode" :size="18" class="mr-2" />
         <Edit3 v-else :size="18" class="mr-2" />
-        <span>{{ editMode ? 'Modifica Layout' : 'Blocca Layout' }}</span>
+        <span>{{ editMode ? "Blocca Layout" : "Modifica Layout" }}</span>
       </button>
     </div>
- -->
-    <GridContainer 
-  v-model:layout="layout" 
-  :is-editable="editMode"
->
-  <template #default="{ item }: any">
-    <component
-      v-if="item && item.i"
-      :is="getComponent(item.i)"
-      class="w-full h-full p-4"
-    />
-  </template>
-</GridContainer>
+    <GridContainer v-model:layout="layout" :is-editable="editMode">
+      <template #default="{ item }: any">
+        <component
+          v-if="item && item.i"
+          :is="getComponent(item.i)"
+          class="w-full h-full p-4"
+        />
+      </template>
+    </GridContainer>
   </main>
 </template>
